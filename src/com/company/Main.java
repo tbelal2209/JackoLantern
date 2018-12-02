387d521d0c696d9e943b1a22b78a8ad49d1bc0bb
tbelal2209 <43207470+tbelal2209@users.noreply.github.com>
2018-12-02 18:04:49 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,26 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         String[][] face = new String[5][8];
-        JackOLantern face1 = new JackOLantern(face);
+        FaceDrawing face1 = new FaceDrawing(face);
 
-        face1.fill("X");
-        face1.edit(" ", 0, 0);
-        face1.edit(" ", 4, 7);
-        face1.edit(" ", 0, 7);
-        face1.edit(" ", 4, 0);
-
-        face.edit("0", 1, 2);
-        face1.edit("0", 1, 5);
-        face1.edit("^", 2, 3);
-        face1.edit("^", 2, 4);
-        face1.edit("|", 3, 1);
-        face1.edit("|", 3, 2);
-        face1.edit("_", 3, 3);
-        face1.edit("_", 3, 4);
-        face1.edit("|", 3, 5);
-        face1.edit("|", 3, 6);
+        face1.fill("Y");
+        face1.edit(" ", 0,0);
+        face1.edit(" ", 4,7);
+        face1.edit(" ", 0,7);
+        face1.edit(" ", 4,0);
+        face1.edit("0", 1,2);
+        face1.edit("0", 1,5);
+        face1.edit("o", 2,3);
+        face1.edit("o", 2,4);
+        face1.edit("-", 3,1);
+        face1.edit("-", 3,2);
+        face1.edit("-", 3,3);
+        face1.edit("-", 3,4);
+        face1.edit("-", 3,5);
+        face1.edit("-", 3, 6);
         System.out.println(face1);
     }
 }
